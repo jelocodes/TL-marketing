0b8c2f74b0e621a1c357fdf3fc162d7b582df19d
jelocodes <michaelangelo.yambao@gmail.com>
2015-03-11 01:46:28 -0400

    Created Terms of Purchase modal box, and tab functionality

--- dev/javascript/app.js
+++ dev/javascript/app.js
@@ -10,6 +10,35 @@ $(document).ready(function() {
 		$('input.upgrade').val('BUSINESS');
 	}); 
 
+	$('.uno').click(function(){
+		$('.uno').addClass('active');
+		$('.seoTitle').text('Search Engine Optimization');
+		$('.second').removeClass('active');
+		$('.third').removeClass('active');
+		$("a[href='#highimpact']").attr('href', '#more');
+		$("a[href='#social']").attr('href', '#more');
+	});    
+	$('.second').click(function(){
+		$('.second').addClass('active');
+		$('.seoTitle').text('Direct Response Paid Search');
+		$('.uno').removeClass('active');
+		$('.third').removeClass('active');
+		$("a[href='#more']").attr('href', '#highimpact');
+		$("a[href='#social']").attr('href', '#highimpact');
+	});    
+	$('.third').click(function(){
+		$('.third').addClass('active');
+		$('.seoTitle').text('Social Lead Generation');
+		$('.uno').removeClass('active');
+		$('.second').removeClass('active');
+		$("a[href='#highimpact']").attr('href', '#social');
+		$("a[href='#more']").attr('href', '#social');
+	}); 
+
+	$('#checkbox').click(function(){
+		$('#terms').addAttr('checked');
+	});
+
 	$('.submitButton').click(function(){
  
 	$.post("send.php", $("#upgrade").serialize(),  function(data) {   });
