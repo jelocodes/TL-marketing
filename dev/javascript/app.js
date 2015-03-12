4159dbb361d27105eff4a6f4288e5e3563072e27
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2015-03-12 14:03:25 -0400

    added form validation and began making page responsive

--- dev/javascript/app.js
+++ dev/javascript/app.js
@@ -11,6 +11,20 @@ $(document).ready(function() {
 		$('input.budget').val(type);
 	})
 
+	$('#email').focusout(function(){
+
+	     $('#email').filter(function(){
+	        var emil=$('#email').val();
+	        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+	         if( !emailReg.test( emil ) ) {
+	            alert('Please enter valid email');
+	         } else {
+	            alert('Thank you for your valid email');
+	            }
+	         })
+	     });
+	});
+
 
 	$('.uno').click(function(){
 		$('.uno').addClass('active');
@@ -141,8 +155,7 @@ $(document).ready(function() {
 			$('#thankyouModal').attr('src', thankyouUrl);
 		},'JSON')
 		return false;
-	})
-
+	});
 
 });
 
